b3079970322f60a3203ec010f590b8356cf3f47b
Beatriz Marcondes Cesar <copatropa32@gmail.com>
2026-08-13 00:53:02 -0300

    Atualiza API com fetch nativo da FreeToGame

--- api/jogos.js
+++ api/jogos.js
@@ -1,9 +1,8 @@
-const axios = require('axios');
-
 module.exports = async (req, res) => {
     try {
-        const response = await axios.get('https://www.freetogame.com/api/games');
-        const jogosApi = response.data.slice(0, 12); // Pega os 12 primeiros jogos
+        const response = await fetch('https://www.freetogame.com/api/games');
+        const data = await response.json();
+        const jogosApi = data.slice(0, 12); // Pega os 12 primeiros jogos
 
         const jogosFormatados = jogosApi.map(jogo => ({
             name: jogo.title,
@@ -13,6 +12,6 @@ module.exports = async (req, res) => {
 
         return res.status(200).json(jogosFormatados);
     } catch (error) {
-        return res.status(500).json({ error: 'Erro ao buscar jogos' });
+        return res.status(500).json({ error: 'Erro ao buscar jogos', details: error.message });
     }
 };
